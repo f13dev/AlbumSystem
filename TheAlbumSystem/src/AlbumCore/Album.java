de1c44e8ed38ed6b4eeb9026ac8cbe735e705861
James Valentine <jv@f13dev.com>
2016-06-22 10:38:56 +0100

    First code iteration for the Album class

--- TheAlbumSystem/src/AlbumCore/Album.java
+++ TheAlbumSystem/src/AlbumCore/Album.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package AlbumCore;
 
@@ -10,5 +8,39 @@ package AlbumCore;
  * @author jamesvalentine
  */
 public class Album {
+    
+    /* Stores the linked Artist object */
+    private Artist artist;
+    /* Stores the album titile */
+    private String title;
+    
+    /**
+     * Initialises a new Album object using the arguments anArtist and aTitle.
+     * @param anArtist
+     * @param aTitle 
+     */
+    Album(Artist anArtist, String aTitle)
+    {
+        this.artist = anArtist;
+        this.title = aTitle;
+    }
+    
+    /**
+     * Returns the artist attribute of the receiver.
+     * @return artist
+     */
+    public Artist getArtist()
+    {
+        return this.artist;
+    }
+    
+    /**
+     * Returns the name attribute of the receiver.
+     * @return title
+     */
+    public String getTitle()
+    {
+        return this.title;
+    }
     
 }
